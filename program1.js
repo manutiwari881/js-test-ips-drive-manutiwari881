4b1431dbf38ee51e9e0a1dfbeb65d0dc071984d2
manutiwari881 <164267790+manutiwari881@users.noreply.github.com>
2024-04-26 15:51:14 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,38 +1,30 @@
-function smallestMissingPositiveInteger(arr) {
-    const n = arr.length;
-
-    // Step 1: Move all non-positive integers to the end of the array
-    let j = 0;
-    for (let i = 0; i < n; i++) {
-        if (arr[i] <= 0) {
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-            j++;
-        }
-    }
-
-    // Step 2: Consider only positive integers
-    arr = arr.slice(j);
-    const m = arr.length;
-
-    // Step 3: Mark arr[i] as visited by making arr[arr[i]-1] negative
-    for (let i = 0; i < m; i++) {
-        const index = Math.abs(arr[i]) - 1;
-        if (index < m && arr[index] > 0) {
-            arr[index] = -arr[index];
-        }
-    }
-
-    // Step 4: Find the first positive integer in the modified array
-    for (let i = 0; i < m; i++) {
-        if (arr[i] > 0) {
-            return i + 1;
-        }
-    }
-
-    // Step 5: If all integers are present, return m + 1
-    return m + 1;
+function smallestMissingPositiveInteger(nums) {
+  const n = nums.length;
+  
+   let j = 0;
+  for (let i = 0; i < n; i++) {
+      if (nums[i] <= 0) {
+          [nums[i], nums[j]] = [nums[j], nums[i]];
+          j++;
+      }
+  }
+  
+   const x = nums.slice(j);
+  
+   for (let i = 0; i < x.length; i++) {
+      const index = Math.abs(x[i]) - 1;
+      if (index < x.length && x[index] > 0) {
+          x[index] = -x[index];
+      }
+  }
+  
+   for (let i = 0; i < x.length; i++) {
+      if (x[i] > 0) {
+          return i + 1;
+      }
+  }
+  
+   return x.length + 1;
 }
 
-// Example usage:
-const arr = [3, 4, -1, 1];
-console.log(smallestMissingPositiveInteger(arr));
+module.exports = smallestMissingPositiveInteger;
