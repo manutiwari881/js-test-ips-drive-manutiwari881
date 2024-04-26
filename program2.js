39a73b39a9f5e7e9b3c8fb9e9c0af25e098ff7d3
manutiwari881 <164267790+manutiwari881@users.noreply.github.com>
2024-04-26 14:05:25 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -2,6 +2,20 @@ function longestSubstring(s) {
 
 
     // Implementation of longestSubstring function
+    let longestSubstringlength =0;
+    for (let i=0 ; i<s.length; i++){
+        let currentStringSet = new Set();
+        for (let x=i ; x<s.length;x++){
+            if (currentStringSet.has(s[x])){
+                break;
+            }
+       else{
+        currentStringSet.add(s[x]);
+       }
+        }
+    longestSubstringlength=Math.max(longestSubstringlength,currentStringSet.size);
+    }
+    return longestSubstringlength
 }
 
 module.exports = { longestSubstring };
